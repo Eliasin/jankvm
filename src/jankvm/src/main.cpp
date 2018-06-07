0afc480b26e39af3f85efb8cb77cdbe8f64d6d5f
Eliasin <steven.pham.8.2000@gmail.com>
2018-06-07 15:42:48 -0400

    Factor out utility functions into seperate 'utils' module

--- src/jankvm/src/main.cpp
+++ src/jankvm/src/main.cpp
@@ -1,54 +1,10 @@
 #include <string>
 #include <fstream>
 #include <iostream>
-#include <cerrno>
-#include <cstring>
 #include <cinttypes>
-#include <limits>
 
 #include <argparse.hpp>
-#include <boost/predef/other/endian.h>
-
-
-template <typename T, size_t N>
-void reverseArray(T array[N]) {
-	static_assert(std::numeric_limits<std::ptrdiff_t>::max() >= 0, "std::ptrdiff_t max size invariant violated");
-	static_assert(sizeof(size_t) >= sizeof(std::ptrdiff_t), "size_t must be at least equal in size to std::ptrdiff_t");
-	static_assert(N < static_cast<size_t>(std::numeric_limits<std::ptrdiff_t>::max()), "Narrowing conversion from size_t to std::ptrdiff_t");
-	for (std::ptrdiff_t i = 0; i < static_cast<std::ptrdiff_t>(N / 2); i++) {
-		T temp = array[i];
-		array[i] = array[static_cast<std::ptrdiff_t>(N) - i - 1];
-		array[static_cast<std::ptrdiff_t>(N) - i - 1] = temp;
-	}
-}
-
-
-template <typename T, size_t N>
-T bytesToType(const char bytes[N]) {
-	static_assert(N > 0, "Attempted cast of zero bytes to type");
-	static_assert(sizeof(T) == N, "Attempted cast from bytes to type of incompatible size");
-	T result;
-
-	char endianAdjustedBytes[N];
-	memcpy(endianAdjustedBytes, bytes, N);
-
-	if (BOOST_ENDIAN_LITTLE_BYTE) {
-		reverseArray<char, N>(endianAdjustedBytes);
-	}
-
-	memcpy(&result, endianAdjustedBytes, N);
-	return result;
-}
-
-
-bool tryRead(std::istream& in, char* buffer, size_t n) {
-	if (!in.read(buffer, n)) {
-		std::cerr << "Error: " << strerror(errno) << std::endl;
-		return false;
-	}
-	return true;
-}
-
+#include <data_utils.hpp>
 
 bool checkForMagicValue(std::ifstream& file) {
 	constexpr int magic = 0xCAFEBABE;
