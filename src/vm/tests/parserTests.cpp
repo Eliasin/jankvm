e30dd317e38131457026a39b531c0484442d11d2
Eliasin <steven.pham.8.2000@gmail.com>
2019-05-25 02:18:47 -0400

    Add float parsing tests

--- src/vm/tests/parserTests.cpp
+++ src/vm/tests/parserTests.cpp
@@ -13,6 +13,7 @@ TEST_CASE("constant pool parsing", "[constant pool]") {
 	auto constantPool = parser.parseConstantPool(f);
 	REQUIRE(constantPool);
 
-	REQUIRE(std::get<uint32_t>(constantPool->at(0)) == 2);
+	REQUIRE(std::get<int32_t>(constantPool->at(0)) == 2);
 	REQUIRE(std::get<float>(constantPool->at(1)) - 16.f < 0.001);
+	REQUIRE(std::get<int64_t>(constantPool->at(2)) == 42949672970);	
 }
